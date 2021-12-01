01cfcc1f6a6683185c5ece9fa1312fac2c5a8f1c
Jake Ledoux <contactjakeledoux@gmail.com>
2021-12-01 07:39:46 -0500

    Refactored solution

--- 2021/day-01/src/main.rs
+++ 2021/day-01/src/main.rs
@@ -1,5 +1,7 @@
 #![allow(dead_code)]
 
+use itertools::Itertools;
+
 const SAMPLE: &str = include_str!("../sample.txt");
 const INPUT: &str = include_str!("../input.txt");
 
@@ -11,25 +13,18 @@ fn part_one(numbers: &[usize]) -> usize {
 }
 
 fn part_two(numbers: &[usize]) -> usize {
-    let numbers: Vec<usize> = numbers
-        .windows(3)
-        .map(|window| window.into_iter().sum())
-        .collect();
-
     numbers
-        .as_slice()
-        .windows(2)
-        .filter(|window| window[1] > window[0])
+        .windows(3)
+        .map(|window| window.iter().sum::<usize>())
+        .tuple_windows()
+        .filter(|(a, b)| b > a)
         .count()
 }
 
 fn main() {
-    let numbers: Vec<_> = INPUT
-        .split('\n')
-        .filter_map(|s| match s.trim() {
-            "" => None,
-            _ => s.trim().parse::<usize>().ok(),
-        })
+    let numbers: Vec<usize> = INPUT
+        .lines()
+        .filter_map(|s| s.trim().parse().ok())
         .collect();
 
     dbg!(part_one(&numbers));
