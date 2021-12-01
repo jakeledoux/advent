8a3de6c81891a7e7495b1666a0d6013533ae947c
Jake Ledoux <contactjakeledoux@gmail.com>
2021-12-01 09:25:07 -0500

    Reuse pt 1 code

--- 2021/day-01/src/main.rs
+++ 2021/day-01/src/main.rs
@@ -5,28 +5,24 @@ use itertools::Itertools;
 const SAMPLE: &str = include_str!("../sample.txt");
 const INPUT: &str = include_str!("../input.txt");
 
-fn part_one(numbers: &[usize]) -> usize {
-    numbers
-        .windows(2)
-        .filter(|window| window[1] > window[0])
-        .count()
+fn part_one<T: Clone + Ord, I: Iterator<Item = T>>(numbers: I) -> usize {
+    numbers.tuple_windows().filter(|(a, b)| b > a).count()
 }
 
 fn part_two(numbers: &[usize]) -> usize {
-    numbers
-        .windows(3)
-        .map(|window| window.iter().sum::<usize>())
-        .tuple_windows()
-        .filter(|(a, b)| b > a)
-        .count()
+    part_one(
+        numbers
+            .windows(3)
+            .map(|window| window.iter().sum::<usize>()),
+    )
 }
 
 fn main() {
-    let numbers: Vec<usize> = INPUT
+    let numbers = INPUT
         .lines()
         .filter_map(|s| s.trim().parse().ok())
-        .collect();
+        .collect_vec();
 
-    dbg!(part_one(&numbers));
+    dbg!(part_one(numbers.iter()));
     dbg!(part_two(&numbers));
 }
