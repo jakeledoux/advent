1bb3d980044c9bb8adf320dff37d381b4c0f1fc9
Jake Ledoux <contactjakeledoux@gmail.com>
2021-12-12 12:24:16 -0500

    Refactored to use tests

---
+++ 2021/day-12/src/lib.rs
@@ -0,0 +1,66 @@
+use cached::proc_macro::cached;
+use itertools::Itertools;
+use petgraph::graphmap::UnGraphMap;
+
+type CaveMap<'a> = UnGraphMap<&'a str, ()>;
+
+fn recursive_solver<'a, F: Fn(&str, &[&str]) -> bool>(
+    history: &[&'a str],
+    target: &'a str,
+    map: &'a CaveMap,
+    filter: &F,
+) -> Vec<Vec<&'a str>> {
+    let mut paths = vec![];
+    if let Some(pos) = history.last() {
+        if *pos == target {
+            return vec![history.to_vec()];
+        }
+        paths.extend(
+            map.neighbors(pos)
+                .filter(|neighbor| filter(neighbor, history))
+                .flat_map(|neighbor| {
+                    let new_history = [history, &[neighbor]].concat();
+                    recursive_solver(&new_history, target, map, filter)
+                }),
+        );
+    }
+    paths
+}
+
+fn make_filter(base_limit: usize, secondary_limit: usize) -> impl Fn(&str, &[&str]) -> bool {
+    move |n: &str, history: &[&str]| -> bool {
+        let counts = history
+            .iter()
+            .copied()
+            .filter(|e| e.chars().all(char::is_lowercase))
+            .counts();
+        let limit = if n == "start" || counts.values().contains(&secondary_limit) {
+            base_limit
+        } else {
+            secondary_limit
+        };
+        let instances = counts.get(n).copied().unwrap_or(0);
+        instances < limit
+    }
+}
+
+pub fn part_one(input: &'static str) -> usize {
+    let input = parse_input(input);
+    recursive_solver(&["start"], "end", &input, &make_filter(1, 1)).len()
+}
+
+pub fn part_two(input: &'static str) -> usize {
+    let input = parse_input(input);
+    recursive_solver(&["start"], "end", &input, &make_filter(1, 2)).len()
+}
+
+#[cached]
+pub fn parse_input(input: &'static str) -> CaveMap {
+    input
+        .lines()
+        .map(|edge| edge.split_once('-').unwrap())
+        .fold(CaveMap::new(), |mut map, edge| {
+            map.add_edge(edge.0, edge.1, ());
+            map
+        })
+}
