9e70906894950ae214e525282104a4f799ddee05
Jake Ledoux <contactjakeledoux@gmail.com>
2021-12-15 11:57:18 -0500

    Improved documentation

--- 2021/day-15/src/lib.rs
+++ 2021/day-15/src/lib.rs
@@ -65,33 +65,38 @@ impl CaveMap {
     }
 }
 
-fn dijkstra(map: CaveMap) -> Option<Danger> {
+/// Finds the safest path using Dijkstra and returns its total danger value
+fn safest_path(map: &CaveMap) -> Option<Danger> {
     let (start_pos, end_pos) = ((0, 0), (map.width - 1, map.height - 1));
 
-    let mut min_danger: HashMap<Pos, Danger> = HashMap::new();
+    let mut total_danger: HashMap<Pos, Danger> = map
+        .cells()
+        .into_iter()
+        .map(|(pos, _)| (pos, if pos == start_pos { 0 } else { Danger::MAX }))
+        .collect();
     let mut best_parent: HashMap<Pos, Pos> = HashMap::new();
     let mut queue: Vec<Pos> = vec![start_pos];
 
-    for (pos, _) in map.cells() {
-        min_danger.insert(pos, if pos == start_pos { 0 } else { Danger::MAX });
-    }
-
     while !queue.is_empty() {
+        // Get most promising elem from queue
         let pos = queue.pop().unwrap();
 
         // Found target
         if pos == end_pos {
-            return Some(min_danger[&end_pos]);
+            return Some(total_danger[&end_pos]);
         }
 
         for (neighbor_pos, neighbor_danger) in map.get_adjacent_cells(pos) {
-            let alt = min_danger[&pos] + neighbor_danger;
-            if alt < min_danger[&neighbor_pos] {
-                min_danger.insert(neighbor_pos, alt);
+            // Check if this path to `neighbor` is less dangerest than previous best
+            let alt_total_danger = total_danger[&pos] + neighbor_danger;
+            if alt_total_danger < total_danger[&neighbor_pos] {
+                // Update best path
+                total_danger.insert(neighbor_pos, alt_total_danger);
                 best_parent.insert(neighbor_pos, pos);
 
+                // Queue neighbor for expansion (insert pre-sorted)
                 let idx = queue
-                    .binary_search_by(|other| alt.cmp(&min_danger[other]))
+                    .binary_search_by(|other| alt_total_danger.cmp(&total_danger[other]))
                     .unwrap_or_else(|i| i);
                 queue.insert(idx, neighbor_pos);
             }
@@ -102,31 +107,31 @@ fn dijkstra(map: CaveMap) -> Option<Danger> {
 
 pub fn part_one(input: &'static str) -> usize {
     let map = parse_input(input, 1);
-    dijkstra(map).unwrap()
+    safest_path(&map).unwrap()
 }
 
 pub fn part_two(input: &'static str) -> usize {
     let map = parse_input(input, 5);
-    dijkstra(map).unwrap()
+    safest_path(&map).unwrap()
 }
 
-fn parse_input(input: &'static str, upscale: usize) -> CaveMap {
+fn parse_input(input: &'static str, scale: usize) -> CaveMap {
     let width = input.lines().next().unwrap().len();
     let height = input.lines().count();
 
     CaveMap::new(
-        &(0..upscale)
+        &(0..scale)
             .flat_map(|y| {
                 input.lines().flat_map(move |line| {
                     let cells = line.chars().map(|c| c.to_digit(10).unwrap() as usize);
-                    (0..upscale).flat_map(move |x| {
+                    (0..scale).flat_map(move |x| {
                         cells.clone().map(|c| (c + x + y - 1) % 9 + 1).collect_vec()
                     })
                 })
             })
             .collect_vec(),
-        width * upscale,
-        height * upscale,
+        width * scale,
+        height * scale,
     )
     .unwrap()
 }
